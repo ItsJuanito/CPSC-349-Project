7b0aeeefd70269a411de8241a382bd3c7c6db543
Anthony Goossens <31324413+Creative-Yorkii@users.noreply.github.com>
2020-07-09 19:07:48 -0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -13,13 +13,13 @@
   </head>
   <body>
     <header>
-      <a href="index.html" class="header-brand">RateMyNetlfix</a>
+      <a href="welcome.php" class="header-brand">RateMyNetlfix</a>
       <nav>
         <ul>
           <li><a href="about.html">About</a></li>
           <li><a href="contact.html">Contact</a></li>
         </ul>
-        <a href="gallery.php" class="header-cases">Movies</a>
+        <a href="gallery.php" class="header-cases">My Account</a>
       </nav>
     </header>
     <main>
